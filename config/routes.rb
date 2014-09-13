8b227609ce6f1bea61ca496ff63b329b778e721b
Sean_Cheetham <skc010@bucknell.edu>
2014-09-12 20:18:35 -0400

    Class 3 Lab 04

--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,6 @@ Bookstore::Application.routes.draw do
   get "/books/new"=>"books#new",as:'new_book'
   get "/books/:id"=>"books#show",as:'book'
   post "/books"=>"books#create"
+  get "/books/:id/edit"=>"books#edit", as:'edit_book'
+  patch "/books/:id"=>"books#update"
 end
