d8bda76167cff41035ad033477031ff6ca2bebc6
Sean_Cheetham <skc010@bucknell.edu>
2014-09-25 19:05:28 -0400

    Lab04 part 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,11 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
-		@book.save
-		redirect_to @book
+		if @book.save
+			redirect_to @book
+		else
+			render :new
+		end
 	end
 
 	def edit 
