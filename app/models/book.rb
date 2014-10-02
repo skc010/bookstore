30e8a005d2f1405686fe0cc3558bca14017c6f44
Sean_Cheetham <skc010@bucknell.edu>
2014-10-02 18:35:53 -0400

    Class 5 Lab1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
 	scope :bargins, -> { where('price < 10.00')}
 	scope :by, ->(author) { where('author = ?', author)}
+
+	has_many :reviews
 	validates :title, :author, :pages, :price, presence: true
 	validates :pages,
 		numericality: { only_integer: true, greater_than_or_equal_to: 0},
