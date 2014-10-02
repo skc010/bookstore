a6f37fbc4c47ea6663f178ccd29f1f224d28a49b
Sean_Cheetham <skc010@bucknell.edu>
2014-10-02 19:28:50 -0400

    Class 5 Lab3

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,14 @@
 module BooksHelper
 	def formatted_time(time)
-	time.strftime("%B %d, %Y at %l:%M %p")
+		time.strftime("%B %d, %Y at %l:%M %p")
+	end
+
+	def format_average_stars(book)
+		average = book.average_stars
+		if average
+			pluralize(number_with_precision(average, precision, 1), 'star')
+		else
+			'No Reviews'
+		end
 	end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,8 @@ class Book < ActiveRecord::Base
 	validates :price,
 		numericality: {greater_than_or_equal_to: 0},
 		if: "price.present?"
+
+	def average_stars
+		reviews.average(:stars)
+	end
 end
